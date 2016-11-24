fab0469f2db82f1bd9652ddfb85d43523d4fbad8
Domagoj Katavic <domo1503@gmail.com>
2016-11-24 22:21:49 +0000

    CustomersController test

---
+++ test/unit/controllers/CustomersController.test.js
@@ -0,0 +1,44 @@
+// CustomersController.test.js
+
+var expect = require('chai').expect;
+var request = require('supertest');
+
+describe('CustomersController', function() {
+
+  describe('GET /customers', function() {
+
+    var tests = [{
+      sortBy: 'first_name'
+    },{
+      sortBy: 'last_name'
+    },{
+      sortBy: 'created_at'
+    }];
+
+    it('Should list all customers');
+
+    tests.forEach((test) => {
+      it(`Should list all customers sorted by ${test.sortBy}`);
+    });
+
+  });
+
+  describe('POST /customer', function() {
+
+    it('Shold create customer');
+
+  });
+
+  describe('DELETE /customer/:id', function() {
+
+    it('Should delete customer');
+
+  });
+
+  describe('PUT /customer/:id', function() {
+
+    it('Should edit customer');
+
+  });
+
+});
