1e055da94b7bf2e02f4b0c052b0a78cd93789b75
Domagoj Katavic <domo1503@gmail.com>
2016-11-25 01:11:37 +0000

    generated customer api

---
+++ api/controllers/CustomersController.js
@@ -0,0 +1,51 @@
+/**
+ * CustomersController
+ *
+ * @description :: Server-side logic for managing customers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `CustomersController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CustomersController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CustomersController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CustomersController.list()`
+   */
+  list: function (req, res) {
+    return res.json({
+      todo: 'list() is not implemented yet!'
+    });
+  }
+};
+
